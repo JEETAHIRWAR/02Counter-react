315d697886f8d1cb2be3c33d2e46342498a9ffc7
JEET AHIRWAR <jeetahirwar664@gmail.com>
2024-04-29 13:11:08 +0530

    add: initial files

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/02counter/',
   plugins: [react()],
+  base: '/02Counter-react/'
 })
